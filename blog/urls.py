556ff0794c45df51c2fa1c61869e672246a0fe0b
shalter <ubuntuphp@126.com>
2020-09-17 23:53:17 +0800

    modifyed the index and detail

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 import blog.views
 
 urlpatterns = [
-    path('content', blog.views.article_content)
+    path('content', blog.views.article_content),
+    path('index', blog.views.get_index_page),
+    path('detail', blog.views.get_detail_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,20 @@ def article_content(request):
     return_str = 'title: %s, breif_content: %s, content: %s, article_id: %s, publish_date: %s' % (title,breif_content,content,article_id,publish_date)
     return HttpResponse(return_str)
 
+def get_index_page(request):
+    all_article = Article.objects.all()
+    return render(request, 'blog/index.html',
+                {
+                    'article_list': all_article
+                }
+                )
+
+def get_detail_page(request):
+    curr_article = Article.objects.all()[0]
+    section_list = curr_article.content.split('\n')
+    return render(request, 'blog/detail.html',
+                {
+                    'curr_article': curr_article,
+                    'section_list': section_list
+                }
+                )
