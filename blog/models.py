fc90f4479357af8294a226d2bab139a50b96c051
shalter <ubuntuphp@126.com>
2020-09-17 15:54:48 +0800

    add Article and append blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    # 文章唯一ID
+    article_id = models.AutoField(primary_key=True)
+    # 标题
+    title = models.CharField(max_length=200)
+    # 摘要
+    brief_content = models.TextField()
+    # 内容
+    content = models.TextField()
+    # 发布日期
+    publish_date = models.DateTimeField(auto_now=True)
