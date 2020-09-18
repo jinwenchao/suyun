4cc30428560426632a217a6b51e0efe1c6f2fbd7
shalter <ubuntuphp@126.com>
2020-09-18 14:13:20 +0800

    modified the link of previous and next

--- blog/views.py
+++ blog/views.py
@@ -25,14 +25,32 @@ def get_index_page(request):
 def get_detail_page(request, article_id):
     all_article = Article.objects.all()
     curr_article = None
-    for article in all_article:
+    previous_index = 0
+    next_index = 0
+    previous_article = None
+    next_article = None
+    for index, article in enumerate(all_article):
+        if index == 0:
+            previous_index = 0
+            next_index = index + 1
+        elif index == len(all_article) - 1:
+            previous_index = index -1
+            next_index = index
+        else:
+            previous_index = index - 1
+            next_index = index + 1
+
         if article.article_id == article_id:
             curr_article = article
+            previous_article = all_article[previous_index]
+            next_article = all_article[next_index]
             break
     section_list = curr_article.content.split('\n')
     return render(request, 'blog/detail.html',
                 {
                     'curr_article': curr_article,
-                    'section_list': section_list
+                    'section_list': section_list,
+                    'previous_article': previous_article,
+                    'next_article': next_article
                 }
                 )
